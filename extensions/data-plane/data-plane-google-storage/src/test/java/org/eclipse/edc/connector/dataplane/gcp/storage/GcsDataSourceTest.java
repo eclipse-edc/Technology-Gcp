4dd2eb3369692e7c89538bc001994b0c4dc13706
Andrea Marchini <man8pr@gmail.com>
2023-11-23 09:48:00 +0100

    fix: GCS DataSource checks source blob when creating part stream, not when opening the stream

---
+++ extensions/data-plane/data-plane-google-storage/src/test/java/org/eclipse/edc/connector/dataplane/gcp/storage/GcsDataSourceTest.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (c) 2023 Google LLC
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Google LCC - Initial implementation
+ *
+ */
+
+package org.eclipse.edc.connector.dataplane.gcp.storage;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import org.eclipse.edc.spi.monitor.Monitor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GcsDataSourceTest {
+
+    Monitor monitor = mock(Monitor.class);
+    Storage storageClient = mock(Storage.class);
+    String bucketName = "TestBucketName";
+    String blobName = "TestBlobName";
+    GcsDataSource dataSource = GcsDataSource.Builder.newInstance()
+            .storageClient(storageClient)
+            .monitor(monitor)
+            .bucketName(bucketName)
+            .blobName(blobName)
+            .build();
+
+    BlobId blobId = BlobId.of(bucketName, blobName);
+
+
+    @Test
+    void openPartStream_failsIfBlobIsNull() {
+        when(storageClient.get(blobId))
+                .thenReturn(null);
+
+        var partStream = dataSource.openPartStream();
+
+        assertThat(partStream.failed()).isTrue();
+    }
+
+    @Test
+    void openPartStream_failsIfBlobDoesntExist() {
+        var blob = mock(Blob.class);
+
+        when(blob.exists())
+                .thenReturn(false);
+
+        when(storageClient.get(blobId))
+                .thenReturn(blob);
+
+        var partStream = dataSource.openPartStream();
+
+        assertThat(partStream.failed()).isTrue();
+    }
+
+    @Test
+    void openPartStream_succeedsIfBlobExists() {
+        var blob = mock(Blob.class);
+
+        when(blob.exists())
+                .thenReturn(true);
+
+        when(storageClient.get(blobId))
+                .thenReturn(blob);
+
+        var partStream = dataSource.openPartStream();
+
+        assertThat(partStream.succeeded()).isTrue();
+    }
+}
