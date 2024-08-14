0b4bb5897ee1aeaf266d83c9d9b23803dde286af
ndr_brt <andrea.bertagnolli@gmail.com>
2024-08-14 14:33:03 +0200

    removed unnecessary version override

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,30 +18,18 @@ plugins {
 
 val techGcpScmUrl: String by project
 val techGcpScmConnection: String by project
-val annotationProcessorVersion: String by project
-val metaModelVersion: String by project
 
 buildscript {
     dependencies {
-        val edcGradlePluginsVersion: String by project
-        classpath("org.eclipse.edc.edc-build:org.eclipse.edc.edc-build.gradle.plugin:${edcGradlePluginsVersion}")
+        val version: String by project
+        classpath("org.eclipse.edc.edc-build:org.eclipse.edc.edc-build.gradle.plugin:$version")
     }
 }
 
 allprojects {
     apply(plugin = "org.eclipse.edc.edc-build")
 
-    // configure which version of the annotation processor to use. defaults to the same version as the plugin
-    configure<org.eclipse.edc.plugins.autodoc.AutodocExtension> {
-        processorVersion.set(annotationProcessorVersion)
-        outputDirectory.set(project.buildDir)
-    }
-
     configure<org.eclipse.edc.plugins.edcbuild.extensions.BuildExtension> {
-        versions {
-            // override default dependency versions here
-            metaModel.set(metaModelVersion)
-        }
         pom {
             scmUrl.set(techGcpScmConnection)
             scmConnection.set(techGcpScmConnection)
